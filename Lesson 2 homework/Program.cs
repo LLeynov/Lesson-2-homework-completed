058e614d01da3f5a7d3baf7663f43a0745133a63
Lev <lleynov2018@mail.ru>
2022-03-03 23:40:28 +0300

    Выполнен первый пункт домашнего задания.

--- Lesson 2 homework/Program.cs
+++ Lesson 2 homework/Program.cs
@@ -10,6 +10,22 @@ namespace Lesson_2_homework
     {
         static void Main(string[] args)
         {
+            //Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+
+            string maxtemp;
+            Console.WriteLine("Пожалуйста,введите максимальную температуру за сутки.");
+            maxtemp = Console.ReadLine();
+            int a = Convert.ToInt32(maxtemp);
+            Console.WriteLine("Максимальная температура за сутки была " + maxtemp + " градусов . ");
+
+            string mintemp;
+            Console.WriteLine("Пожалуйста,введите минимальную температуру за сутки.");
+            mintemp = Console.ReadLine();
+            int b = Convert.ToInt32(mintemp);
+            Console.WriteLine("Минимальная температура за сутки была " + mintemp + " градусов . ");
+
+            Console.WriteLine("Средняя температура за сутки составляет : " + ((a + b) / 2));
+
         }
     }
 }
